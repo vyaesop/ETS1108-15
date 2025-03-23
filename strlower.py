82f8d98fd9c773064eec43e011d639740a6ba9ca
vyaesop <100709552+vyaesop@users.noreply.github.com>
2025-03-23 15:16:48 +0300

    up

--- strlower.py
+++ strlower.py
@@ -2,7 +2,7 @@ user_input = "the quick brown fox jumps over the lazy dog"
 keyword = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
 
 # Convert both strings to lowercase for case-insensitive comparison
-if user_input.lower() == keyword.lower():
+if user_input.lower() == keyword.upper():
     print("The strings match!")
 else:
     print("The strings are different.")
