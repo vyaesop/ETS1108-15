7b9b740b3826e34640c011c43abfa6edacdf1491
vyaesop <100709552+vyaesop@users.noreply.github.com>
2025-03-26 16:15:23 +0300

    up

---
+++ strup.py
@@ -0,0 +1,8 @@
+user_input = "the quick brown fox jumps over the lazy dog"
+keyword = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+
+# Convert both strings to lowercase for case-insensitive comparison
+if user_input.upper() == keyword.upper():
+    print("The strings match!")
+else:
+    print("The strings are different.")
